a0eeb8a955a85ed7a34c8307a7a83e2b195e3f0b
“Pallavi <pallavibharwani@icloud.comgit config --global user.name Pallavi Bharwanigit config --global user.email pallavibharwani@icloud.com>
2022-06-10 20:40:08 +1000

    finished

--- carol-house-furniture-workspace.js
+++ carol-house-furniture-workspace.js
@@ -14,3 +14,16 @@ function openNav() {
 function back(){
     document.location.href = "cart.html";
   }
+
+
+// js for add to cart button
+const add_cart_btn = 
+document.getElementById("notification_btn");
+
+const notification = 
+document.getElementById("close");
+
+add_cart_btn.addEventListener("click", ()=>
+{
+    notification.classList.add("show");
+})
