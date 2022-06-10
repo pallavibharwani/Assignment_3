e43fc4a6a7f94921a830c18ea1694cdef1ea193f
“Pallavi <pallavibharwani@icloud.comgit config --global user.name Pallavi Bharwanigit config --global user.email pallavibharwani@icloud.com>
2022-06-10 14:58:20 +1000

    updated

---
+++ carol-house-furniture-workspace.js
@@ -0,0 +1,16 @@
+/* Open */
+function openNav() {
+    document.getElementById("hamburger").style.display = "block";
+  }
+  
+  /* Close */
+  function closeNav() {
+    document.getElementById("hamburger").style.display = "none";
+  }
+
+
+// function for back button 
+
+function back(){
+    document.location.href = "cart.html";
+  }
